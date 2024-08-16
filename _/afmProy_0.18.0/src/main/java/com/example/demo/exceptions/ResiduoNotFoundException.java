6f3fc01a2bb84d4bd059467cad139c9492c071a1
Alejandro <alejandro.fdez.martinez@gmail.com>
2024-08-16 11:31:47 +0200

    Copia de seguridade

--- _/afmProy_0.18.0/src/main/java/com/example/demo/exceptions/ResiduoNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.exceptions;
-
-public class ResiduoNotFoundException extends RuntimeException {
-    public ResiduoNotFoundException(Long id) {
-        super("No se puede encontrar el residuo con ID: " + id);
-    }
-}
